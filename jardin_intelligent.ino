e526396ceedcce1a9f52cd0c34aee42ba491611a
LouisGabrielBC <151726078+LouisGabrielBC@users.noreply.github.com>
2023-12-12 18:43:20 +0100

    Update jardin_intelligent.ino

--- jardin_intelligent.ino
+++ jardin_intelligent.ino
@@ -7,7 +7,9 @@
 #include "m_management.h"
 #include <Arduino.h>
 
+//Bouton poussoir pour choisir le type de plante 
 #define  BoutonPin      D6
+
 
 barette_LED BL;
 temperature_sensor TS;
@@ -18,17 +20,18 @@ moisture_sensor MS;
 P_Tropical TROPICAL ;
 P_Moderee MODEREE ;
 P_Aride DESERT ;
-
+//définition d'un tableau contenant les trois types de plantes possibles et d'un pointeur pour sélectionner 
 int itplante = 0;
 plante* tabplante[3]={&MODEREE,&DESERT,&TROPICAL};
+//création des objets de management initiaux
 s_management SM(&TS,&BL,&E,&TROPICAL);
 m_management MM(&MS,&TROPICAL);
 
 int buttonState ;
 
 void setup(){
-  pinMode(BoutonPin,INPUT);
-  Serial.begin(9600);
+  pinMode(BoutonPin,INPUT); //définition du mode de pin pour le bouton poussoir
+  //Initialisation de tous les capteurs et actionneurs
   MM.PompeInit();
   SM.LEDinit();
   SM.ECRANinit();
@@ -39,7 +42,7 @@ void setup(){
 
 
 void loop(){
-
+// A chaque appui, on change de plante 
   buttonState = digitalRead(BoutonPin);
 
   if (buttonState==1){
@@ -52,7 +55,7 @@ void loop(){
     MM.setplante(tabplante[itplante]);
 
     }
-
+ //A chaque boucle on actualise les capteurs et les actionneurs
     SM.LEDmanagement();
     SM.ECRANmanagement();
     MM.ComPompe();
