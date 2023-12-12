a800b88f115bd38f3e7b00c60b626c4917bd45b6
LouisGabrielBC <151726078+LouisGabrielBC@users.noreply.github.com>
2023-12-12 18:16:52 +0100

    Update m_management.h

--- include/m_management.h
+++ include/m_management.h
@@ -7,15 +7,17 @@
 
 #define PompePin         D8
 
+//la classe m_management gère la pompe, elle arrose la plante quand son taux d'humidité est infèrieur à son taux minimum
+
     class m_management {
     private :
       moisture_sensor *M;
       plante *P;
     public :
     m_management(moisture_sensor *MS, plante *PL);
-    void PompeInit();
-    void ComPompe();
-    void setplante(plante* p);
+    void PompeInit(); //on donne le pin de la pompe au programme
+    void ComPompe(); //on met en marche la pompe si le taux d'humidité est infèrieur au taux minimum sinon on arrête la pompe
+    void setplante(plante* p); //on peut changer la plante à arroser n'importe quand
   };
 
 #endif
