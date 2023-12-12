bec65d8f44ae4c84331d9ab796063b6f87cc9d9d
LouisGabrielBC <151726078+LouisGabrielBC@users.noreply.github.com>
2023-12-12 18:21:08 +0100

    Update s_management.h

--- include/s_management.h
+++ include/s_management.h
@@ -6,6 +6,7 @@
 #include "LED.h"
 #include "Plantes.h"
 
+//la classe s_management indique à la barette et à l'écran ce qu'ils doivent afficher en fonction de la température et de l'humidité captées par le capteursT&H ainsi que par la plante qui lui est attribuée
 
 class s_management{
   private:
@@ -18,12 +19,12 @@ class s_management{
     s_management(temperature_sensor* s,  barette_LED* b, ecran_LED* e, plante* p);
 
     void LEDinit();
-    void LEDmanagement();
+    void LEDmanagement();//on indique le nombre de led à allumées, la couleur puis on refresh
 
     void ECRANinit();
-    void ECRANmanagement();
+    void ECRANmanagement();//on construit et on indique le message à afficher, on donne la couleur puis on refresh
 
-    void setplante (plante* p);
+    void setplante (plante* p);//on peut changer la plante n'importe quand
 };
 
 #endif
